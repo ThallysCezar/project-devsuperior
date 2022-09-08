fc4eeaaeb0cba9b617dbccfc21a55ad546e8225a
Thallys Cezar <thalys_cezar@hotmail.com>
2022-09-08 17:40:29 -0300

    refactor: add API Test

---
+++ backend/src/main/java/com/projetodevsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.projetodevsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetodevsuperior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
